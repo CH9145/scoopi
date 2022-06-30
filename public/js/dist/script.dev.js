9d1d8dad92a0ee4549a8127dffefc665708f5685
CH9145 <whlj123@gmail.com>
2022-06-30 17:45:07 +0800

    0.0.6 (2022-06-30) 首頁手機板nav

---
+++ public/js/dist/script.dev.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var btnNavToggle = document.getElementById('btn-nav-toggle');
+var primaryNavigation = document.getElementById('primary-navigation');
+btnNavToggle.addEventListener('click', openNav); // 函式名稱
+
+function openNav() {
+  // document.getElementById('primary-navigation')
+  console.log('點按鈕打開主導覽');
+
+  if (primaryNavigation.classList.contains('active') == true) {
+    // 假如主導覽有active類別時執行
+    primaryNavigation.classList.remove('active');
+    document.getElementById('btn-nav-toggle').textContent = img(src = "public/images/icon-menu.svg", alt = "");
+  } else {
+    // 假如主導覽沒有active類別時執行
+    primaryNavigation.classList.add('active');
+    document.getElementById('btn-nav-toggle').textContent = img(src = "public/images/icon-menu-close.svg", alt = "");
+  }
+}
