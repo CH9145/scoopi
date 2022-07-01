2cfdea926e95b983cc704bed97ba0f632a8686e3
CH9145 <whlj123@gmail.com>
2022-07-01 13:31:51 +0800

    0.0.8 (2022-07-01) 首頁完，開始做產品頁

--- public/js/dist/script.dev.js
+++ public/js/dist/script.dev.js
@@ -1,20 +1,8 @@
 "use strict";
 
-var btnNavToggle = document.getElementById('btn-nav-toggle');
-var primaryNavigation = document.getElementById('primary-navigation');
-btnNavToggle.addEventListener('click', openNav); // 函式名稱
-
-function openNav() {
-  // document.getElementById('primary-navigation')
-  console.log('點按鈕打開主導覽');
-
-  if (primaryNavigation.classList.contains('active') == true) {
-    // 假如主導覽有active類別時執行
-    primaryNavigation.classList.remove('active');
-    document.getElementById('btn-nav-toggle').textContent = img(src = "public/images/icon-menu.svg", alt = "");
-  } else {
-    // 假如主導覽沒有active類別時執行
-    primaryNavigation.classList.add('active');
-    document.getElementById('btn-nav-toggle').textContent = img(src = "public/images/icon-menu-close.svg", alt = "");
-  }
-}
+$('#btn-nav-switch').on('click', function () {
+  $('nav').toggleClass('active');
+}); ///submenu
+//- $('nav>ul>li').on('click',function(){
+//-     $(this).find('.submenu').toggleClass('active')
+//- })
--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,8 @@
-let btnNavToggle = document.getElementById('btn-nav-toggle')
-let primaryNavigation = document.getElementById('primary-navigation')
+$('#btn-nav-switch').on('click',function(){
+    $('nav').toggleClass('active')
+})
 
-btnNavToggle.addEventListener('click', openNav)
-
-// 函式名稱
-function openNav() {
-    // document.getElementById('primary-navigation')
-    console.log('點按鈕打開主導覽')
-    if (primaryNavigation.classList.contains('active') == true) {
-        // 假如主導覽有active類別時執行
-        primaryNavigation.classList.remove('active')
-        document.getElementById('btn-nav-toggle').textContent = img(src = "public/images/icon-menu.svg", alt = "")
-
-    } else {
-        // 假如主導覽沒有active類別時執行
-        primaryNavigation.classList.add('active')
-        document.getElementById('btn-nav-toggle').textContent = img(src = "public/images/icon-menu-close.svg", alt = "")
-    }
-}
+///submenu
+//- $('nav>ul>li').on('click',function(){
+//-     $(this).find('.submenu').toggleClass('active')
+//- }) 
