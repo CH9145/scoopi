df7e6e11901d234f2630bae642858721142d4252
CH9145 <whlj123@gmail.com>
2022-07-02 14:33:33 +0800

    0.1.2 (2022-07-02) 聯絡end 開始做about

--- public/js/dist/script.dev.js
+++ public/js/dist/script.dev.js
@@ -2,7 +2,26 @@
 
 $('#btn-nav-switch').on('click', function () {
   $('nav').toggleClass('active');
-}); ///submenu
+});
+var offsetTop;
+var product1Top = $('.product-section-1').offset().top;
+$(window).scroll(function () {
+  offsetTop = $(window).scrollTop(); //- //卷軸高度>=900會出現按鈕
+
+  if (offsetTop >= 900) {
+    $('.page-top').addClass('active');
+  } else {
+    $('.page-top').removeClass('active');
+  }
+}); //scroll end
+//- //點page-top回到最上方
+
+$('.page-Top').on('click', function () {
+  $('html', body).animate({
+    scrollTop: 0
+  });
+}); //.page-top end
+///submenu
 //- $('nav>ul>li').on('click',function(){
 //-     $(this).find('.submenu').toggleClass('active')
 //- })
