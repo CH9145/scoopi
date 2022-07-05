17e66e8d0566fed318d5cd7dc0807fb785a45e56
CH9145 <whlj123@gmail.com>
2022-07-05 12:59:40 +0800

    0.2.1 (2022-07-05) 加wow js效果

--- public/js/dist/script.dev.js
+++ public/js/dist/script.dev.js
@@ -2,33 +2,32 @@
 
 $('#btn-nav-switch').on('click', function () {
   $('nav').toggleClass('active');
-}); // 卷軸回到top
+}); // //最新消息頁 摺疊
 
-var offsetTop;
-var product1Top = $('.product-section-1').offset().top;
-$(window).scroll(function () {
-  offsetTop = $(window).scrollTop(); //- //卷軸高度>=900會出現按鈕
-
-  if (offsetTop >= 900) {
-    $('.page-top').addClass('active');
-  } else {
-    $('.page-top').removeClass('active');
-  }
-}); //scroll end
-//- //點page-top回到最上方
-
-$('.page-Top').on('click', function () {
-  $('html', body).animate({
-    scrollTop: 0
-  });
-}); //.page-top end
-//最新消息頁 摺疊
-
-$('.news-inner').on('click', function () {
+$('.news-outer').on('click', function () {
   $(this).toggleClass('active');
   $(this).siblings().removeClass('active');
-}); //最新消息摺疊 end
-///submenu
+}); // }) //最新消息摺疊 end
+// // 卷軸回到top
+// let offsetTop
+// let product1Top = $('.product-section-1').offset().top
+//     $(window).scroll(function(){
+//         offsetTop = $(window).scrollTop()
+//         //- //卷軸高度>=900會出現按鈕
+//         if( offsetTop >= 900){
+//             $('.page-top').addClass('active')
+//         }else{
+//             $('.page-top').removeClass('active')
+//         }
+//     }) //scroll end
+// //- //點page-top回到最上方
+// $('.page-Top').on('click',function(){
+//     $('html',body).animate({scrollTop:0})
+// }) //.page-top end
+
+$(window).scroll(function () {}); ///submenu
 //- $('nav>ul>li').on('click',function(){
 //-     $(this).find('.submenu').toggleClass('active')
-//- })
+//- }) 
+
+new WOW().init();
--- public/js/script.js
+++ public/js/script.js
@@ -3,33 +3,43 @@ $('#btn-nav-switch').on('click',function(){
 })
 
 
-// 卷軸回到top
-let offsetTop
-let product1Top = $('.product-section-1').offset().top
-    $(window).scroll(function(){
-        offsetTop = $(window).scrollTop()
-        //- //卷軸高度>=900會出現按鈕
-        if( offsetTop >= 900){
-            $('.page-top').addClass('active')
-        }else{
-            $('.page-top').removeClass('active')
-        }
-    }) //scroll end
-//- //點page-top回到最上方
-$('.page-Top').on('click',function(){
-    $('html',body).animate({scrollTop:0})
-}) //.page-top end
 
-
-//最新消息頁 摺疊
-$('.news-inner').on('click', function(){
+// //最新消息頁 摺疊
+$('.news-outer').on('click', function(){
     $(this).toggleClass('active')
     $(this).siblings().removeClass('active')
-}) //最新消息摺疊 end
+})
+// }) //最新消息摺疊 end
 
 
+// // 卷軸回到top
+// let offsetTop
+// let product1Top = $('.product-section-1').offset().top
+//     $(window).scroll(function(){
+//         offsetTop = $(window).scrollTop()
+//         //- //卷軸高度>=900會出現按鈕
+//         if( offsetTop >= 900){
+//             $('.page-top').addClass('active')
+//         }else{
+//             $('.page-top').removeClass('active')
+//         }
+//     }) //scroll end
+// //- //點page-top回到最上方
+// $('.page-Top').on('click',function(){
+//     $('html',body).animate({scrollTop:0})
+// }) //.page-top end
+
+
+
+
+$(window).scroll(function(){
+
+})
 
 ///submenu
 //- $('nav>ul>li').on('click',function(){
 //-     $(this).find('.submenu').toggleClass('active')
 //- }) 
+
+
+new WOW().init()
