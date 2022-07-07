c20046d44b68c3d6b25128bd222522dbeb3770c1
CH9145 <whlj123@gmail.com>
2022-07-07 10:42:44 +0800

    0.2.6 (2022-07-07) NEWS卷軸(無作用

--- public/js/dist/script.dev.js
+++ public/js/dist/script.dev.js
@@ -36,96 +36,59 @@ $(window).on('load', function () {
   $('.loading-overlay').addClass('active');
 }); //window load end
 // // 卷軸回到top
-// let offsetTop
-// let product1Top = $('.product-section-1').offset().top
-//     $(window).scroll(function(){
-//         offsetTop = $(window).scrollTop()
-//         //- //卷軸高度>=900會出現按鈕
-//         if( offsetTop >= 900){
-//             $('.page-top').addClass('active')
-//         }else{
-//             $('.page-top').removeClass('active')
-//         }
-//     }) //scroll end
-//
-// //- //點page-top回到最上方
-// $('.page-Top').on('click',function(){
-//     $('html',body).animate({scrollTop:0})
-// }) //.page-top end
-//
 
+var offsetTop;
+var newsTop = $('main.news').offset().top;
 $(window).scroll(function () {
-  offsetTop = $(window).scrollTop();
-  gsap.to(".deco-1", {
-    y: offsetTop / 10,
-    duration: 5
+  offsetTop = $(window).scrollTop(); //- //卷軸高度>=900會出現按鈕
+
+  if (offsetTop >= 900) {
+    $('.page-top').addClass('active');
+  } else {
+    $('.page-top').removeClass('active');
+  }
+}); //scroll end
+//- //點page-top回到最上方
+
+$('.page-Top').on('click', function () {
+  $('html,body').animate({
+    scrollTop: 0
   });
-  gsap.to(".deco-2", {
-    y: -offsetTop / 10,
-    duration: 3
-  });
-  gsap.to(".deco-3", {
-    y: offsetTop / 10,
-    duration: 2
-  });
-  gsap.to(".deco-4", {
-    y: -offsetTop / 10,
-    duration: 5
-  });
-  gsap.to(".deco-5", {
-    y: offsetTop / 10,
-    duration: 3
-  });
-  gsap.to(".deco-6", {
-    y: -offsetTop / 10,
-    duration: 2
-  });
-}); //
+}); //.page-top end
+// 捲軸事件
+// $(window).scroll(function () {
+//     offsetTop = $(window).scrollTop()
+//     gsap.to(".deco-1", { y: offsetTop / 10, duration: 5 })
+//     gsap.to(".deco-2", { y: -offsetTop / 10, duration: 3 })
+//     gsap.to(".deco-3", { y: offsetTop / 10, duration: 2 })
+//     gsap.to(".deco-4", { y: -offsetTop / 10, duration: 5 })
+//     gsap.to(".deco-5", { y: offsetTop / 10, duration: 3 })
+//     gsap.to(".deco-6", { y: -offsetTop / 10, duration: 2 })
+// })
+//
 // 自適應的卷軸動畫 start
 //
-
-function gsapAnimate() {
-  if ($(window).width() >= 768) {
-    $(window).scroll(function () {
-      offsetTop = $(window).scrollTop();
-      gsap.to(".deco-1", {
-        y: offsetTop / 10,
-        duration: 5
-      });
-      gsap.to(".deco-2", {
-        y: -offsetTop / 10,
-        duration: 3
-      });
-      gsap.to(".deco-3", {
-        y: offsetTop / 10,
-        duration: 2
-      });
-      gsap.to(".deco-4", {
-        y: -offsetTop / 10,
-        duration: 5
-      });
-      gsap.to(".deco-5", {
-        y: offsetTop / 10,
-        duration: 3
-      });
-      gsap.to(".deco-6", {
-        y: -offsetTop / 10,
-        duration: 2
-      });
-    }); // scroll end
-  } else {
-    $(window).scroll(function () {
-      offsetTop = $(window).scrollTop();
-      gsap.to(".deco-1", {
-        y: offsetTop / 1,
-        duration: 1
-      });
-    });
-  }
-} ///submenu
+// function gsapAnimate() {
+//     if ($(window).width() >= 768) {
+//         $(window).scroll(function () {
+//             offsetTop = $(window).scrollTop()
+//             gsap.to(".deco-1", { y: offsetTop / 10, duration: 5 })
+//             gsap.to(".deco-2", { y: -offsetTop / 10, duration: 3 })
+//             gsap.to(".deco-3", { y: offsetTop / 10, duration: 2 })
+//             gsap.to(".deco-4", { y: -offsetTop / 10, duration: 5 })
+//             gsap.to(".deco-5", { y: offsetTop / 10, duration: 3 })
+//             gsap.to(".deco-6", { y: -offsetTop / 10, duration: 2 })
+//         }) // scroll end
+//         } else {
+//         $(window).scroll(function () {
+//             offsetTop = $(window).scrollTop()
+//             gsap.to(".deco-1", { y: offsetTop / 1, duration: 1 })
+//         })
+//     }
+// // }
+///submenu
 //- $('nav>ul>li').on('click',function(){
 //-     $(this).find('.submenu').toggleClass('active')
 //- }) 
-
 
 new WOW().init();
--- public/js/script.js
+++ public/js/script.js
@@ -42,58 +42,59 @@ $(window).on('load',function(){
 }) //window load end
 
 // // 卷軸回到top
-// let offsetTop
-// let product1Top = $('.product-section-1').offset().top
-//     $(window).scroll(function(){
-//         offsetTop = $(window).scrollTop()
-//         //- //卷軸高度>=900會出現按鈕
-//         if( offsetTop >= 900){
-//             $('.page-top').addClass('active')
-//         }else{
-//             $('.page-top').removeClass('active')
-//         }
-//     }) //scroll end
-//
-// //- //點page-top回到最上方
-// $('.page-Top').on('click',function(){
-//     $('html',body).animate({scrollTop:0})
-// }) //.page-top end
-//
+let offsetTop
+let newsTop = $('main.news').offset().top
+    $(window).scroll(function(){
+        offsetTop = $(window).scrollTop()
+        //- //卷軸高度>=900會出現按鈕
+        if( offsetTop >= 900){
+            $('.page-top').addClass('active')
+        }else{
+            $('.page-top').removeClass('active')
+        }
+    }) //scroll end
+
+//- //點page-top回到最上方
+
+$('.page-Top').on('click',function(){
+    $('html,body').animate({scrollTop:0})
+}) //.page-top end
+
+// 捲軸事件
 
 
 
-$(window).scroll(function () {
-    offsetTop = $(window).scrollTop()
-    gsap.to(".deco-1", { y: offsetTop / 10, duration: 5 })
-    gsap.to(".deco-2", { y: -offsetTop / 10, duration: 3 })
-    gsap.to(".deco-3", { y: offsetTop / 10, duration: 2 })
-    gsap.to(".deco-4", { y: -offsetTop / 10, duration: 5 })
-    gsap.to(".deco-5", { y: offsetTop / 10, duration: 3 })
-    gsap.to(".deco-6", { y: -offsetTop / 10, duration: 2 })
-})
 
-
+// $(window).scroll(function () {
+//     offsetTop = $(window).scrollTop()
+//     gsap.to(".deco-1", { y: offsetTop / 10, duration: 5 })
+//     gsap.to(".deco-2", { y: -offsetTop / 10, duration: 3 })
+//     gsap.to(".deco-3", { y: offsetTop / 10, duration: 2 })
+//     gsap.to(".deco-4", { y: -offsetTop / 10, duration: 5 })
+//     gsap.to(".deco-5", { y: offsetTop / 10, duration: 3 })
+//     gsap.to(".deco-6", { y: -offsetTop / 10, duration: 2 })
+// })
 //
 // 自適應的卷軸動畫 start
 //
-function gsapAnimate() {
-    if ($(window).width() >= 768) {
-        $(window).scroll(function () {
-            offsetTop = $(window).scrollTop()
-            gsap.to(".deco-1", { y: offsetTop / 10, duration: 5 })
-            gsap.to(".deco-2", { y: -offsetTop / 10, duration: 3 })
-            gsap.to(".deco-3", { y: offsetTop / 10, duration: 2 })
-            gsap.to(".deco-4", { y: -offsetTop / 10, duration: 5 })
-            gsap.to(".deco-5", { y: offsetTop / 10, duration: 3 })
-            gsap.to(".deco-6", { y: -offsetTop / 10, duration: 2 })
-        }) // scroll end
-        } else {
-        $(window).scroll(function () {
-            offsetTop = $(window).scrollTop()
-            gsap.to(".deco-1", { y: offsetTop / 1, duration: 1 })
-        })
-    }
-}
+// function gsapAnimate() {
+//     if ($(window).width() >= 768) {
+//         $(window).scroll(function () {
+//             offsetTop = $(window).scrollTop()
+//             gsap.to(".deco-1", { y: offsetTop / 10, duration: 5 })
+//             gsap.to(".deco-2", { y: -offsetTop / 10, duration: 3 })
+//             gsap.to(".deco-3", { y: offsetTop / 10, duration: 2 })
+//             gsap.to(".deco-4", { y: -offsetTop / 10, duration: 5 })
+//             gsap.to(".deco-5", { y: offsetTop / 10, duration: 3 })
+//             gsap.to(".deco-6", { y: -offsetTop / 10, duration: 2 })
+//         }) // scroll end
+//         } else {
+//         $(window).scroll(function () {
+//             offsetTop = $(window).scrollTop()
+//             gsap.to(".deco-1", { y: offsetTop / 1, duration: 1 })
+//         })
+//     }
+// // }
 
 
     ///submenu
